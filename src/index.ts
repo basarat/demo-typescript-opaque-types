8d620d1ffc45caebd109e294a02ee47314b26317
Basarat Ali Syed <basaratali@gmail.com>
2022-05-18 16:21:41 +1000

    wrap

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
-async function main() {
-  
+export type AccountNumber = number & { _: 'AccountNumber' };
+export type AccountBalance = number & { _: 'AccountBalance' };
+
+const makeAccountNumber =
+  (accountNumber: number): AccountNumber => accountNumber as AccountNumber;
+const makeAccountBalance =
+  (accountBalance: number): AccountBalance => accountBalance as AccountBalance;
+
+function setupAccount(accountNumber: AccountNumber, accountBalance: AccountBalance) {
+  // ... setup the account
 }
 
-main();
+let accountNumber: AccountNumber = makeAccountNumber(1337);
+let accountBalance: AccountBalance = makeAccountBalance(100);
+
+setupAccount(accountNumber, accountBalance);
+
+// accountNumber = accountBalance;
+// accountBalance = accountNumber;
